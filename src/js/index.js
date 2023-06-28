9c26f9fccff4f9cfbbccf8a9065d6434f6ef0768
King Nodens <judith.zamora.go@gmail.com>
2023-06-28 00:43:07 -0600

    contry button
    asignacion de tareas pendientes

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,14 @@
 import eventsApi from '../js/eventsApi';
 // hago una referencia al elemento del formulario y al campo de entrada
 const form = document.getElementById('search-form');
-const input = form.querySelector('input');
+//const input = form.querySelector('input');
 const startBtn = document.getElementById('start-btn');
 const chooseBtn = document.getElementById('choose-btn');
 
 let data = [];
 
-/*====================================================================================*/
-/*---------------------------- ejemplos de evento de api -----------------------------*/
-/*====================================================================================*/
-
-/*
-eventsApi
-  .getByKey('keyword') // en keyWord colocamos la palabra del evento q queremos buscar
-  .then(result => {
-    // Hacer algo con el resultado de la búsqueda
-    console.log(result);
-  })
-  .catch(error => {
-    // Manejar cualquier error que ocurra
-    console.error(error);
-  });*/
+const countryCode = document.querySelector('#choose').value; // Código del país que deseas buscar
+const pageNumber = 2; // Número de página que deseas obtener
 
 /*====================================================================================*/
 /*------------------------ CARGAMOS MAS IMAGENES CO9N SCROLL -------------------------*/
@@ -30,25 +17,20 @@ eventsApi
 /*
 let currentPage = 0;
 let isLoading = false;
-
 // Función para cargar eventos
 function loadEvents() {
   if (isLoading) {
     return; // Evitar llamadas duplicadas mientras se está cargando
   }
   isLoading = true;
-
   eventsApi
     .getRandom(currentPage)
     .then(result => {
       const newEvents = result._embedded.events;
-
       if (newEvents.length > 0) {
-
         // Si hay nuevos eventos, se agregan al array existente
         data = [...data, ...newEvents];
         console.log(data);
-        
         // Mostrar los nuevos eventos en la página
         newEvents.forEach(item => {
           document.getElementById('gallery').innerHTML += `
@@ -76,7 +58,6 @@ function loadEvents() {
             </li>
           `;
         });
-
         isLoading = false;
       } else {
         // No hay más eventos, deshabilitar la carga
@@ -89,66 +70,66 @@ function loadEvents() {
     }
   );
 }
-
 // Función para manejar el evento de scroll
 function handleScroll() {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
   if (scrollTop + clientHeight >= scrollHeight - 100) {
     // Cuando se llega al final de la página, cargar más eventos
     loadEvents();
   }
 }
-
 // Agregar el evento de scroll
 document.addEventListener('scroll', handleScroll);
-
 // Cargar los eventos iniciales
 loadEvents();
 */
-
 /* --------------------------------------------------------------------------------------*/
 /* --- En esta parte se hace el primer cargado de elementos para mostar en la pagina ----*/
 /* --------------------------------------------------------------------------------------*/
 
-eventsApi // objeto del archivo eventsApi.js
+eventsApi
   .getRandom()
   .then(result => {
-    data = result._embedded.events;
-    data.map(item => {
-      document.getElementById('gallery').innerHTML += `
-      <li class="gallery__item">
+    const data = result._embedded.events;
+    const gallery = document.getElementById('gallery');
+    gallery.innerHTML = ''; // Limpia el contenido existente antes de agregar los nuevos elementos
+    data.forEach(item => {
+      const imageUrl =
+        item.images && item.images[4] && item.images[4].url
+          ? item.images[4].url
+          : '';
+      const venueCity =
+        item._embedded &&
+        item._embedded.venues &&
+        item._embedded.venues[0] &&
+        item._embedded.venues[0].city
+          ? item._embedded.venues[0].city.name
+          : '';
+      const listItem = document.createElement('li');
+      listItem.classList.add('gallery__item');
+      listItem.innerHTML = `
         <a href="" class="gallery__link">
-          <img class="gallery__img" src="${item.images[4].url}"> <br/>
+          <img class="gallery__img" src="${imageUrl}" alt=""> <br/>
         </a>
         <span class="gallery__name">${item.name}</span> <br/>
         <span class="gallery__date">${item.dates.start.localDate}</span> <br/>
         <span class="gallery__city">
-        <svg width="6" height="9" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3 0C1.34581 0 0 1.40339 0 3.12836C0 5.29645 3.00295 9 3.00295 9C3.00295 9 6 5.18983 6 3.12836C6 1.40339 4.65424 0 3 0ZM3.90516 4.04434C3.65558 4.30455 3.32781 4.43469 3 4.43469C2.67224 4.43469 2.34437 4.30455 2.09489 4.04434C1.59577 3.52392 1.59577 2.67709 2.09489 2.15662C2.33658 1.90448 2.65807 1.76561 3 1.76561C3.34193 1.76561 3.66337 1.90453 3.90516 2.15662C4.40428 2.67709 4.40428 3.52392 3.90516 4.04434Z" fill="white"/>
-      </svg>
-        ${
-          item._embedded &&
-          item._embedded.venues &&
-          item._embedded.venues[0] &&
-          item._embedded.venues[0].city
-            ? item._embedded.venues[0].city.name
-            : null
-        }
+          <svg width="6" height="9" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 0C1.34581 0 0 1.40339 0 3.12836C0 5.29645 3.00295 9 3.00295 9C3.00295 9 6 5.18983 6 3.12836C6 1.40339 4.65424 0 3 0ZM3.90516 4.04434C3.65558 4.30455 3.32781 4.43469 3 4.43469C2.67224 4.43469 2.34437 4.30455 2.09489 4.04434C1.59577 3.52392 1.59577 2.67709 2.09489 2.15662C2.33658 1.90448 2.65807 1.76561 3 1.76561C3.34193 1.76561 3.66337 1.90453 3.90516 2.15662C4.40428 2.67709 4.40428 3.52392 3.90516 4.04434Z" fill="white"/>
+          </svg>
+          ${venueCity}
         </span> <br/>
-      </li>
       `;
+      gallery.appendChild(listItem);
     });
     console.log(data);
   })
   .catch(error => {
     console.log(error);
   });
-
 /* --------------------------------------------------------------------------------------*/
 /*--- aca cargamos data o elementos desde el boton buscar con una palab ra de busqueda --*/
 /* --------------------------------------------------------------------------------------*/
-
 function loadData(keyword) {
   document.getElementById('gallery').innerHTML = '';
   eventsApi
@@ -157,7 +138,6 @@ function loadData(keyword) {
       result;
       console.log(result._embedded.events);
       data = result._embedded.events;
-
       data.map(item => {
         document.getElementById('gallery').innerHTML += `
         <li class="gallery__item">
@@ -185,9 +165,49 @@ function loadData(keyword) {
       console.error(error);
     });
 }
-
+/* --------------------------------------------------------------------------------------*/
+/* -------------------le hago pruebas a eventos llamdos por country ---------------------*/
+/* --------------------------------------------------------------------------------------*/
+function loadCountry(countryCode) {
+  document.getElementById('gallery').innerHTML = '';
+  eventsApi
+    .getByCountry(countryCode)
+    .then(result => {
+      result;
+      console.log(result._embedded.events);
+      data = result._embedded.events;
+      data.map(item => {
+        document.getElementById('gallery').innerHTML += `
+        <li class="gallery__item">
+          <img class="gallery__img" src="${item.images[4].url}"> <br/>
+          <span class="gallery__name">${item.name}</span> <br/>
+          <span class="gallery__date">${item.dates.start.localDate}</span> <br/>
+          <span class="gallery__city">
+          <svg width="6" height="9" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3 0C1.34581 0 0 1.40339 0 3.12836C0 5.29645 3.00295 9 3.00295 9C3.00295 9 6 5.18983 6 3.12836C6 1.40339 4.65424 0 3 0ZM3.90516 4.04434C3.65558 4.30455 3.32781 4.43469 3 4.43469C2.67224 4.43469 2.34437 4.30455 2.09489 4.04434C1.59577 3.52392 1.59577 2.67709 2.09489 2.15662C2.33658 1.90448 2.65807 1.76561 3 1.76561C3.34193 1.76561 3.66337 1.90453 3.90516 2.15662C4.40428 2.67709 4.40428 3.52392 3.90516 4.04434Z" fill="white"/>
+        </svg>
+          ${
+            item._embedded &&
+            item._embedded.venues &&
+            item._embedded.venues[0] &&
+            item._embedded.venues[0].city
+              ? item._embedded.venues[0].city.name
+              : null
+          }
+          </span> <br/>
+        </li>
+        `;
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+chooseBtn.addEventListener('click', () => {
+  const chooseValue = document.querySelector('#choose').value;
+  console.log('Pugliese', chooseValue);
+  loadCountry(chooseValue);
+});
 startBtn.addEventListener('click', event => {
   loadData(document.querySelector('#search').value);
-  // const resultado = document.querySelector('.resultado');
-  // resultado.textContent = `Te gusta el sabor ${event.target.value}`;
 });
