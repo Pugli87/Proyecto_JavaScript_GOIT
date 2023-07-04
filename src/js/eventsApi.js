cbf54fa26f4ffd5fed184247f770dedb8e8450d6
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-07-04 06:33:24 -0500

    Update eventsApi.js
    creacion de funcion para mejorar o renderizar codigo  y creacion para return de paginacion

--- src/js/eventsApi.js
+++ src/js/eventsApi.js
@@ -1,5 +1,6 @@
 const API_KEY = 'xtboiYIsAyoQG85vMxNOToDrUrhqLhlf';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
+const BASE_URL2 = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}}&size=16&includeTBA=no&includeTBD=no&sort=random`;
 
 const eventsApi = {
   async getByKey(keyWord, page = 0) {
@@ -7,6 +8,7 @@ const eventsApi = {
       const searchRequest = `${BASE_URL}events.json?&keyword=${keyWord}&apikey=${API_KEY}&size=16&page=${page}&includeTBA=no&includeTBD=no&sort=random`;
       const responce = await fetch(searchRequest);
       const result = await responce.json();
+      pagination = result.page;
       return result;
     } catch (err) {
       return err;
@@ -18,6 +20,7 @@ const eventsApi = {
       const searchRequest = `${BASE_URL}events.json?&countryCode=${countryCode}&apikey=${API_KEY}&size=16&page=${page}&includeTBA=no&includeTBD=no&sort=random`;
       const responce = await fetch(searchRequest);
       const result = await responce.json();
+      pagination = result.page;
       return result;
     } catch (err) {
       return err;
@@ -29,6 +32,7 @@ const eventsApi = {
       const searchRequest = `${BASE_URL}events.json?&keyword=${keyWord}&countryCode=${countryCode}&apikey=${API_KEY}&size=16&page=${page}&includeTBA=no&includeTBD=no&sort=random`;
       const responce = await fetch(searchRequest);
       const result = await responce.json();
+      pagination = result.page;
       return result;
     } catch (err) {
       return err;
@@ -51,6 +55,28 @@ const eventsApi = {
       const searchRequest = `${BASE_URL}events.json?&apikey=${API_KEY}&page=${page}&size=16&includeTBA=no&includeTBD=no&sort=random`;
       const responce = await fetch(searchRequest);
       const result = await responce.json();
+      pagination = result.page;
+      return result;
+    } catch (err) {
+      return err;
+    }
+  },
+
+  async getEvents(page = 0, options) {
+    try {
+      let searchRequest = `${BASE_URL2}&page=${page}`;
+      if (options.keyword) {
+        searchRequest += `&keyword=${options.keyWord}`;
+      }
+      // searchRequest  &keyword=madonna
+      if (options.countryCode) {
+        searchRequest += `&countryCode=${options.countryCode}`;
+      }
+      // searchRequest  &keyword=madonna&countryCode=US
+      const responce = await fetch(searchRequest);
+      const result = await responce.json();
+
+      pagination = result.page;
       return result;
     } catch (err) {
       return err;
