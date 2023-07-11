c208dd1bcee3557aca26035c6e6e93eea2acf6d1
dmartinez316 <david316_valdes@hotmail.com>
2023-07-10 21:58:28 -0500

    Update function export

--- src/js/eventsApi.js
+++ src/js/eventsApi.js
@@ -2,17 +2,18 @@ const API_KEY = 'xtboiYIsAyoQG85vMxNOToDrUrhqLhlf';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
 const BASE_URL2 = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}}&size=16&includeTBA=no&includeTBD=no&sort=random`;
 
+export async function getByKey(keyWord, page = 0) {
+  try {
+    const searchRequest = `${BASE_URL}events.json?&keyword=${keyWord}&apikey=${API_KEY}&size=16&page=${page}&includeTBA=no&includeTBD=no&sort=random`;
+    const responce = await fetch(searchRequest);
+    const result = await responce.json();
+    return result;
+  } catch (err) {
+    return err;
+  }
+}
+
 const eventsApi = {
-  async getByKey(keyWord, page = 0) {
-    try {
-      const searchRequest = `${BASE_URL}events.json?&keyword=${keyWord}&apikey=${API_KEY}&size=16&page=${page}&includeTBA=no&includeTBD=no&sort=random`;
-      const responce = await fetch(searchRequest);
-      const result = await responce.json();
-      return result;
-    } catch (err) {
-      return err;
-    }
-  },
   async getByCountry(countryCode, page = 0) {
     try {
       const searchRequest = `${BASE_URL}events.json?&countryCode=${countryCode}&apikey=${API_KEY}&size=16&page=${page}&includeTBA=no&includeTBD=no&sort=random`;
